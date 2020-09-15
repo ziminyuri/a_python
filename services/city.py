196c3336067334c3d09d3fc2c4d42517ed667ec7
Zimin <zimin@MacBook-Pro-Zimin.local>
2020-09-15 14:56:27 +0300

    add method get_info_of_two_cities with parameter: is_north, make some refactoring

--- services/city.py
+++ services/city.py
@@ -1,5 +1,6 @@
 from typing import Union
 from pytils import translit
+from services.transform import get_dict_from_city_str
 
 
 def get_city_line_by_id(geonameid: int) -> Union[str, bool]:
@@ -21,7 +22,6 @@ def get_city_by_ru_name(ru_name: str) -> Union[str, bool]:
 
     city_line = ''
     for line in file:
-        print(line)
         i_city_name: str = line.split('\t')[1]
         if i_city_name == translit_city_name:
             if city_line == '':
@@ -35,6 +35,26 @@ def get_city_by_ru_name(ru_name: str) -> Union[str, bool]:
 
     else:
         return False
+
+
+def get_northern_city(city_1: str, city_2: str) -> dict:
+    if city_1 and city_2:
+        city_1_dict: dict = get_dict_from_city_str(city_1)
+        city_2_dict: dict = get_dict_from_city_str(city_2)
+
+        if float(city_1_dict['latitude']) > float(city_2_dict['latitude']):
+            return city_1_dict
+        else:
+            return city_2_dict
+
+    elif city_1:
+        return {"Error": "There is no city №2"}
+
+    elif city_2:
+        return {"Error": "There is no city №1"}
+
+    else:
+        return {"Error": "There is no cities with such names"}
 
 
 def _get_population_from_city_line(city: str) -> float:
--- services/transform.py
+++ services/transform.py
@@ -2,26 +2,33 @@ def get_dict_from_city_str(city_line: str) -> dict:
     # Возвращает словарь: ключами являются свойства города
     city_list: list = city_line.split('\t')
 
-    city_dict: dict = {}
-    city_dict['geonameid'] = city_list[0]
-    city_dict['name'] = city_list[1]
-    city_dict['asciiname'] = city_list[2]
-    city_dict['alternatenames'] = city_list[3]
-    city_dict['latitude'] = city_list[4]
-    city_dict['longitude '] = city_list[5]
-    city_dict['feature class'] = city_list[6]
-    city_dict['feature code'] = city_list[7]
-    city_dict['country code'] = city_list[8]
-    city_dict['cc2'] = city_list[9]
-    city_dict['admin1 code'] = city_list[10]
-    city_dict['admin2 code'] = city_list[11]
-    city_dict['admin3 code'] = city_list[12]
-    city_dict['admin4 code'] = city_list[13]
-    city_dict['population'] = city_list[14]
-    city_dict['elevation'] =city_list[15]
-    city_dict['dem'] = city_list[16]
-    city_dict['timezone'] = city_list[17]
-    city_dict['modification date'] = city_list[18].split('\n')[0]
+    return {'geonameid': city_list[0], 'name': city_list[1], 'asciiname': city_list[2],
+                       'alternatenames': city_list[3], 'latitude': city_list[4], 'longitude ': city_list[5],
+                       'feature class': city_list[6], 'feature code': city_list[7], 'country code': city_list[8],
+                       'cc2': city_list[9], 'admin1 code': city_list[10], 'admin2 code': city_list[11],
+                       'admin3 code': city_list[12], 'admin4 code': city_list[13], 'population': city_list[14],
+                       'elevation': city_list[15], 'dem': city_list[16], 'timezone': city_list[17],
+                       'modification date': city_list[18].split('\n')[0]}
 
-    return city_dict
 
+def get_dict_from_2_cities_str(request_name_city_1: str, request_name_city_2: str,
+                               city_1: str = None, city_2: str = None) -> dict:
+    # Возвращает словарь, состоящий из данных о двух городах. Если данных не найдено, то возвращается ошибка об
+    # отсутствии информации о нужном городе
+
+    response_data: dict = {}
+
+    if city_1:
+        response_data1: dict = get_dict_from_city_str(city_1)
+        response_data['1'] = response_data1
+    else:
+        response_data['1'] = 'Error. There is no information about ' + request_name_city_1
+
+    if city_2:
+        response_data2: dict = get_dict_from_city_str(city_2)
+        response_data['2'] = response_data2
+
+    else:
+        response_data['2'] = 'Error. There is no information about ' + request_name_city_2
+
+    return response_data
